862fc1b001408ec8c0461b6c99f3896daacb1965
Aditya Tripathi <adityatripathi.at04@gmail.com>
2023-08-11 23:17:39 +0530

    hello

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
+	site: "https://adityatrips.github.io",
+});
